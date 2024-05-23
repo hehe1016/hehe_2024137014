3f48040b11afe61f30dfb3d20b5abc612383ad4c
hehe1016 <msjhej486@gmail.com>
2024-05-23 15:05:58 +0900

    [Feat] UIManager 까지 진행

--- UnityProject_A_24_01/Assets/Scripts/Game/GameManager.cs
+++ UnityProject_A_24_01/Assets/Scripts/Game/GameManager.cs
@@ -1,6 +1,8 @@
+using System;                                                     //Acticon을 사용기 위해서
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -8,6 +10,12 @@ public class GameManager : MonoBehaviour
     public Transform genTransform;                                 //생성 위치 설정 
     public float timeCheck;                                        //생성 시간 설정 변수 (flaot)
     public bool isGen;                                             //생성 체크 (bbol)
+
+    public int Point;                                              //점수
+    public int BestScore;                                          //최고 점수
+
+    public static event Action<int> OnPointChanged;                // 점수가 변경 되었을 때 이벤트를 발생 시킨다
+    public static event Action<int> OnBestScoreChanged;            //최고 점수가 변경 되었을 때 이벤트를 발생 시킨다
 
     public void GenObject()                                        //생성 관련 변수값 변경 시켜주는 함수 
     {
@@ -18,7 +26,10 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        BestScore = PlayerPrefs.GetInt("BestScore");
         GenObject();
+        OnPointChanged?.Invoke(Point);                //시작할때 점수 한 번 갱신
+        OnBestScoreChanged?.Invoke(BestScore);        //시작할때 최고 점수 한 번 갱신
     }
 
     // Update is called once per frame
@@ -29,8 +40,8 @@ public class GameManager : MonoBehaviour
             timeCheck -= Time.deltaTime;                            //매 프레임 돌아가면서 시간을 감소 시킨다.
             if(timeCheck<0.0f)                                      //0초 이하가 되었을 경우
             {
-                int RandNumber = Random.Range(0, 3);                //0~2의 랜덤 넘버 생성
-                GameObject Temp = Instantiate(circleObject[0]);     //프리팹 생성후 Temp 오브젝트에 넣는다.
+                int RandNumber = UnityEngine.Random.Range(0, 3);                //0~2의 랜덤 넘버 생성
+                GameObject Temp = Instantiate(circleObject[RandNumber]);     //프리팹 생성후 Temp 오브젝트에 넣는다.
                 Temp.transform.position = genTransform.position;    //고정 위치에 생성시킨다.
                 isGen = true;
             }
@@ -41,5 +52,22 @@ public class GameManager : MonoBehaviour
         GameObject Temp = Instantiate(circleObject[index]);        //생성된 과일 오브젝트를 Temp에 넣는다.
         Temp.transform.position = position;                        //Temp 오브젝트의 위치는 함수로 받아온 위치값
         Temp.GetComponent<CircleObject>().Used();                  //생성 되었을 때 사용되었다고 표시 해줘야함
+
+        Point += (int)Mathf.Pow(index, 2) * 10;                    //index의 2승으로 포인트 증가 POW 함수 활용
+        OnPointChanged?.Invoke(Point);                             //포인트가 변경 되었을 때 이벤트에 변경 되었다고 알림
+    }
+
+    public void EndGame()                                          //게임 종료 되었을 때 함수
+    {       
+        int BestScore = PlayerPrefs.GetInt("BestScore");           //기존에 저장된 점수를 불러온다
+
+        if (Point > BestScore)                                     //기존 포인트와 비교한다
+        {
+            BestScore = Point;
+            PlayerPrefs.SetInt("BestScore", Point);                //포인트가 더 클 경우 저장한다.
+            OnBestScoreChanged?.Invoke(BestScore);                 
+        }
+
+        //종료시 해야 할일 나중에 추가
     }
 }
